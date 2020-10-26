b802b1774ddc3ca763f51f22c81fac61d26cd917
poojan <savanipoojan78@gmail.com>
2020-10-26 12:52:34 +0530

    added new RectiveForm

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -10,7 +10,9 @@ import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import {Router,RouterModule, Routes} from '@angular/router'
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
-import {FormsModule} from '@angular/forms'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
 
 const appRouters:Routes=[{
     path:'',
@@ -25,6 +27,12 @@ const appRouters:Routes=[{
     path:'property-detail/:id',
     component:PropertyDetailComponent
   },{
+    path:'user/login',
+    component:UserLoginComponent
+  },{
+    path:'user/register',
+    component:UserRegisterComponent
+  },{
     path:'**',
     component:PropertyListComponent
   }
@@ -37,14 +45,17 @@ const appRouters:Routes=[{
     PropertyListComponent,
     NavBarComponent,
     AddPropertyComponent,
-    PropertyDetailComponent
+    PropertyDetailComponent,
+    UserLoginComponent,
+    UserRegisterComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     CommonModule,
     RouterModule.forRoot(appRouters),
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     HousingService
